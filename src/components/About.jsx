9ecb585e196915c90ca975fbd9623740ca3b3cd7
Parmvir Grewal <parmg100@gmail.com>
2023-04-02 20:05:53 -0400

    Fix padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full s md:h-screen max-h-fit py-20 bg-slate-900 ' >
+    <div name="about" className='w-full s md:h-screen max-h-fit bg-slate-900 pb-20 ' >
         <div className='max-w-7xl p-6  border rounded-lg shadow  bg-slate-800 border-slate-700 mx-auto flex flex-col mx-5'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-slate-400 text-slate-200 px-1 py-1 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white hover:rounded-lg duration-200 '>About</p>
