354902c94383cebda2c21cbdc064925b40bc13de
Parmvir Grewal <parmg100@gmail.com>
2023-04-04 19:14:41 -0400

    Fix bug

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import ReactGA from 'react-ga4';
-
-ReactGA.initialize("G-B15RW0RCTP");
 
 function App() {
   return (
