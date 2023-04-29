3762d87a8ee71e96b98744f4f8e1fd7bc3d82f2d
Parmvir Grewal <parmg100@gmail.com>
2023-04-29 18:15:28 -0400

    Add scroll to top button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import ReactGA from 'react-ga4';
+import ScrollToTop from "react-scroll-up";
+import {BsFillArrowUpSquareFill} from 'react-icons/bs';
 
 ReactGA.initialize("G-B15RW0RCTP");
 
@@ -14,6 +16,11 @@ function App() {
   return (
     
     <div>
+      <ScrollToTop showUnder={800} style={{ bottom:"25px", right:"25px", zIndex:10}}>
+        <span className="bg-white text-slate-300 hover:text-white rounded-lg hover:scale-105 duration-200">
+          <BsFillArrowUpSquareFill size={45}/>
+        </span>
+      </ScrollToTop>
       <NavBar />
       <Home />
       <About/>
