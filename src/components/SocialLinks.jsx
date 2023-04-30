bb1a4b69638b8244ddea86e728bfff99f756fd5d
Parmvir Grewal <parmg100@gmail.com>
2023-04-30 14:06:25 -0400

    change redundant social link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
+import {BsTelephoneOutbound} from 'react-icons/bs';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -40,23 +40,21 @@ const SocialLinks = () => {
             id: 4,
             child: (
                 <>
-                    Resume <BsFillPersonLinesFill size={32}/>
+                    Call <BsTelephoneOutbound size={32}/>
                 </>
             ),
-            href: '/Parmvir_Grewal_Resume.pdf',
+            href: 'tel:647-545-1647',
             style: "rounded-br-md",
-            download: true,
         }
     ]
 
   return (
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
         <ul>
-            {links.map(({id, child, href, style, download}) => (
+            {links.map(({id, child, href, style, }) => (
                 <li key={id} className={`flex justify-between items-center w-40 h-16 px-4 ml-[-100px] hover:ml-[-10px] ${style} hover:rounded-md duration-300 hover:bg-gradient-to-r from-cyan-500 to-blue-500 bg-slate-800 backdrop-blur-md bg-slate-800/60 bg-blur`} >
                 <a href={href} 
                 className="flex justify-between items-center w-full text-slate-300" 
-                download={download} 
                 target='_blank' 
                 rel="noreferrer"
                 data-aos="fade-right">
