9b348e6566bb29ca8b605f52dbbe3c8e93a9aa4c
Parmvir Grewal <parmg100@gmail.com>
2023-04-30 14:29:46 -0400

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,22 @@ import {AiOutlineCloudDownload} from 'react-icons/ai';
 function Header() {
   return (
     <div name="header" className="flex justify-between items-center w-full h-20 px-4 text-white bg-slate-900">
+    
     <div data-aos="fade-up">
-        <img src={logo} alt="" width='50px'  />
+        <img src={logo} alt="" width='50px' />
     </div>
-    <li className='flex text-white w-fit px-3 py-3 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-200' >
+    <div className='text-white bg-slate-800 backdrop-blur-md bg-slate-800/60 z-10 rounded-3xl w-fit '> 
+    <li className='flex w-fit ' >
                 <a href='/Parmvir_Grewal_Resume.pdf'
-                className="flex justify-between items-center w-full text-slate-300" 
+                className="flex justify-between items-center w-full text-slate-300 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white  hover:scale-105 duration-200 rounded-3xl py-3 px-4" 
                 download={true}
                 target='_blank' 
                 rel="noreferrer">
                     <span className="hidden md:block pr-2">Resume/CV</span> <AiOutlineCloudDownload size={25}/>
                 </a>
                 </li>
+                </div>
+                
     </div>
   )
 }
