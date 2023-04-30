35e9782a9b37c43661f5e3fe4dc12e654f8719fd
Parmvir Grewal <parmg100@gmail.com>
2023-04-29 23:41:08 -0400

    change nav appearance

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -57,10 +57,10 @@ const NavBar = () => {
   ];
   return (
     <div id="#nav" className="w-full  fixed bottom-0 z-10">
-      <div className="flex justify-center items-center h-20 mb-5 mx-auto text-white bg-slate-800 bg-blur opacity-90 z-10 rounded-3xl w-fit">
+      <div className="flex justify-center items-center h-20 mb-5 mx-auto text-white bg-slate-800 backdrop-blur-md bg-slate-800/60 z-10 rounded-3xl w-fit">
       <ul className="flex mx-2" data-aos='fade-up' data-aos-anchor="#home">
           {links.map(({id, link, child, name})=> (
-            <li key={id} className=" cursor-pointer text-slate-400"> <Link to={link} smooth duration={500} className="flex justify-between items-center text-slate-500 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white rounded-lg hover:scale-105 duration-200 p-3 mx-2">{child}{name}</Link></li>
+            <li key={id} className=" cursor-pointer text-slate-400"> <Link to={link} smooth duration={500} className="flex justify-between items-center text-slate-300 hover:bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-white rounded-lg hover:scale-105 duration-200 p-3 mx-2">{child}{name}</Link></li>
           ))}
         </ul>
       </div>
