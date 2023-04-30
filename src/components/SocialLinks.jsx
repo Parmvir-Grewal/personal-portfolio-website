9bd7090a106c90df2899626b6d0c9be80c4c339f
Parmvir Grewal <parmg100@gmail.com>
2023-04-30 12:51:03 -0400

    resize social links

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
             id: 1,
             child: (
                 <>
-                    Linkedin <FaLinkedin size={30}/>
+                    Linkedin <FaLinkedin size={32}/>
                 </>
             ),
             href: 'https://www.linkedin.com/in/parmvir-grewall/',
@@ -22,7 +22,7 @@ const SocialLinks = () => {
             id: 2,
             child: (
                 <>
-                    GitHub <FaGithub size={30}/>
+                    GitHub <FaGithub size={32}/>
                 </>
             ),
             href: 'https://github.com/Parmvir-Grewal',
@@ -31,7 +31,7 @@ const SocialLinks = () => {
             id: 3,
             child: (
                 <>
-                    Mail <HiOutlineMail size={30}/>
+                    Mail <HiOutlineMail size={32}/>
                 </>
             ),
             href: 'mailto:parmg100@gmail.com',
@@ -40,7 +40,7 @@ const SocialLinks = () => {
             id: 4,
             child: (
                 <>
-                    Resume <BsFillPersonLinesFill size={30}/>
+                    Resume <BsFillPersonLinesFill size={32}/>
                 </>
             ),
             href: '/Parmvir_Grewal_Resume.pdf',
@@ -53,7 +53,7 @@ const SocialLinks = () => {
     <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
         <ul>
             {links.map(({id, child, href, style, download}) => (
-                <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] ${style} hover:rounded-md duration-300 hover:bg-gradient-to-r from-cyan-500 to-blue-500 bg-slate-800 backdrop-blur-md bg-slate-800/60 bg-blur`} >
+                <li key={id} className={`flex justify-between items-center w-40 h-16 px-4 ml-[-100px] hover:ml-[-10px] ${style} hover:rounded-md duration-300 hover:bg-gradient-to-r from-cyan-500 to-blue-500 bg-slate-800 backdrop-blur-md bg-slate-800/60 bg-blur`} >
                 <a href={href} 
                 className="flex justify-between items-center w-full text-slate-300" 
                 download={download} 
