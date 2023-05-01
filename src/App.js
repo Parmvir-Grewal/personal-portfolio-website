0b1a2adca4ac93d2ad99a68ba7fa7edc55a51b63
Parmvir Grewal <parmg100@gmail.com>
2023-04-30 21:19:55 -0400

    make custom cursor

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import ReactGA from 'react-ga4';
 import Header from "./components/Header";
-
 ReactGA.initialize("G-B15RW0RCTP");
 
 function App() {
+  const cursorDot = document.querySelector("[data-cursor-dot]");
+  const cursorOutline = document.querySelector("[data-cursor-outline]");
+  window.addEventListener("mousemove",function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+   }, {duration: 500, fill: "forwards"})
+  });
   return (
     <div>
+      <div class="cursor-dot" data-cursor-dot></div>
+      <div class="cursor-outline" data-cursor-outline></div>
       <Header />
       <NavBar />
       <Home />
