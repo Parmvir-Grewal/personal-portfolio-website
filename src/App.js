fac381e1fcc1c069bb74e6f41728363178c96cab
Parmvir Grewal <parmg100@gmail.com>
2023-04-30 21:23:37 -0400

    revert changes

--- src/App.js
+++ src/App.js
@@ -11,22 +11,8 @@ import Header from "./components/Header";
 ReactGA.initialize("G-B15RW0RCTP");
 
 function App() {
-  const cursorDot = document.querySelector("[data-cursor-dot]");
-  const cursorOutline = document.querySelector("[data-cursor-outline]");
-  window.addEventListener("mousemove",function(e){
-  const posX = e.clientX;
-  const posY = e.clientY;
-  cursorDot.style.left = `${posX}px`;
-  cursorDot.style.top = `${posY}px`;
-  cursorOutline.animate({
-    left: `${posX}px`,
-    top: `${posY}px`
-   }, {duration: 500, fill: "forwards"})
-  });
   return (
     <div>
-      <div class="cursor-dot" data-cursor-dot></div>
-      <div class="cursor-outline" data-cursor-outline></div>
       <Header />
       <NavBar />
       <Home />
