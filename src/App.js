a97dd6e0b306df1d311934bb971324050509a985
Parmvir Grewal <parmg100@gmail.com>
2023-05-01 20:11:35 -0400

    add preloader

--- src/App.js
+++ src/App.js
@@ -8,32 +8,59 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import ReactGA from 'react-ga4';
 import Header from "./components/Header";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AnimatedCursor from "react-animated-cursor";
+import GridLoader
+from "react-spinners/GridLoader";
+
 ReactGA.initialize("G-B15RW0RCTP");
 
 
 
 
 function App() {
+  const [loading, setLoading] = useState (false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 8000)
+}, [])
   return (
-    <div>
-      <AnimatedCursor
-      innerSize={20}
-      outerSize={35}
-      outerScale={1.7}
-      innerScale={0.8}
-      trailingSpeed={8}
-      color='203,213,225' />
-      <Header />
-      <NavBar />
-      <Home />
-      <About/>
-      <Portfolio />
-      <Experience />
-      <Contact />
-      <Footer />
-      <SocialLinks />
+    <div >
+       <AnimatedCursor
+        innerSize={20}
+        outerSize={35}
+        outerScale={1.7}
+        innerScale={0.8}
+        trailingSpeed={8}
+        color='203,213,225' />
+      {
+        loading ?
+<div className="App"> 
+        <GridLoader
+        size={30}
+        color={"#0369a1"}
+        loading={loading}
+      />
+</div>
+        :
+
+        <div>
+         
+        <Header />
+        <NavBar />
+        <Home />
+        <About/>
+        <Portfolio />
+        <Experience />
+        <Contact />
+        <Footer />
+        <SocialLinks />
+      </div>
+}
+      
     </div>
   );
 }
