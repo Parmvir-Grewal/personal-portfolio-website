103f529ae491be8544fc76523ba00e1089344948
Parmvir Grewal <parmg100@gmail.com>
2023-05-04 17:32:51 -0400

    update cursor styling

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import ReactGA from 'react-ga4';
 import Header from "./components/Header";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import AnimatedCursor from "react-animated-cursor";
 import GridLoader
-from "react-spinners/GridLoader";
+  from "react-spinners/GridLoader";
 
 ReactGA.initialize("G-B15RW0RCTP");
 
@@ -19,48 +19,54 @@ ReactGA.initialize("G-B15RW0RCTP");
 
 
 function App() {
-  const [loading, setLoading] = useState (false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
     }, 1500)
-}, [])
+  }, [])
   return (
     <div >
-       <AnimatedCursor
-        innerSize={20}
-        outerSize={35}
-        outerScale={1.7}
+      <AnimatedCursor
+        innerSize={15}
+        outerSize={39}
+        outerScale={1.3}
         innerScale={0.8}
         trailingSpeed={8}
-        color='203,213,225' />
+        outerStyle={{
+          backgroundColor: 'transparent',
+          border: '3px solid #cbd5e1'
+        }}
+        innerStyle={{
+          backgroundColor: '#cbd5e1'
+        }} />
       {
         loading ?
-<div className="App"> 
-        <GridLoader
-        size={30}
-        color={"#0369a1"}
-        loading={loading}
-      />
-</div>
-        :
+          <div className="App">
+            <GridLoader
+              size={30}
+              color={"#0369a1"}
+              loading={loading}
+            />
+          </div>
+          :
 
-        <div>
-         
-        <Header />
-        <NavBar />
-        <Home />
-        <About/>
-        <Portfolio />
-        <Experience />
-        <Contact />
-        <Footer />
-        <SocialLinks />
-      </div>
-}
-      
+          <div>
+
+            <Header />
+            <NavBar />
+            <Home />
+            <About />
+            <Portfolio />
+            <Experience />
+            <Contact />
+            <Footer />
+            <SocialLinks />
+          </div>
+      }
+
     </div>
   );
 }
